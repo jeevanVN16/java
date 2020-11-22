46e0fda8a64d65cf48a9a2c2abae7ac9a0cfeac7
jeevanvn <36169415+jeevanvn@users.noreply.github.com>
2020-11-22 21:41:14 +0530

    Modified Code

--- Javatest/src/Playground/ReverseString.java
+++ Javatest/src/Playground/ReverseString.java
@@ -3,7 +3,21 @@ package Playground;
 public class ReverseString {
 
 	public static void main(String[] args) {
-		
+		String original="Jeevan";
+		String reverse="";
+		int length = original.length();
+		for(int i=length-1;i>=0;i--) {
+			reverse= reverse+original.charAt(i);
+			
+		}
+		System.out.println("reverse String= "+reverse );
+		//Check For Plaindrome
+		if(original.equals(reverse)) {
+		System.out.println("Given string is Palindrome");
+		}
+		else {
+        System.out.println("Given string is not Palindrome");			
+		}
 	}
 
 }
